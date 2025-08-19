3452bad6bcef23dd34b720a6360dacc9e5e97a29
mrtstg <mrtstgdev@gmail.com>
2025-08-19 09:46:20 +0300

    feat: added database tables

--- deployment-api/src/Database.hs
+++ deployment-api/src/Database.hs
@@ -36,10 +36,62 @@ import           Config
 import           Control.Monad.IO.Class
 import           Control.Monad.Reader
 import           Data.Aeson
+import           Data.Text
 import           Database.Persist.Sql
 import           Database.Persist.TH
+import           Deployment.Models.Deployment
+import           Proxmox.Deploy.Models.Config
+import           Proxmox.Deploy.Models.Config.VM
 
-share [ mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase||]
+share [ mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+DeploymentTemplateData
+  ownerId Text
+  title Text
+  vms [ConfigVM] sqltype=jsonb
+  availableVMs [Text] sqltype=jsonb
+  existingNetworks [Text] sqltype=jsonb
+  UniqueTitle
+  deriving Show Eq
+DeploymentInstanceData
+  Id Text
+  parent DeploymentTemplateDataId OnDeleteRestrict OnUpdateCascade
+  onwerId Text
+  deployConfig DeployConfig
+  state DeploymentStatus
+  logs [Text] sqltype=jsonb
+  deriving Show
+UsedVMID
+  num Int Primary
+  usedBy DeploymentInstanceDataId OnDeleteCascade OnUpdateCascade
+  UniqueNum
+  deriving Show
+UsedDisplay
+  num Int Primary
+  usedBy DeploymentInstanceDataId OnDeleteCascade OnUpdateCascade
+  UniqueNum
+  deriving Show
+|]
+
+instance PersistField DeploymentStatus where
+  toPersistValue = toPersistValueJSON
+  fromPersistValue = fromPersistValueJSON
+
+instance PersistFieldSql DeploymentStatus where
+  sqlType _ = SqlString
+
+instance PersistField DeployConfig where
+  toPersistValue = toPersistValueJSON
+  fromPersistValue = fromPersistValueJSON
+
+instance PersistFieldSql DeployConfig where
+  sqlType _ = SqlOther "JSONB"
+
+instance PersistField ConfigVM where
+  toPersistValue = toPersistValueJSON
+  fromPersistValue = fromPersistValueJSON
+
+instance PersistFieldSql ConfigVM where
+  sqlType _ = SqlOther "JSONB"
 
 doMigration :: SqlPersistT IO ()
 doMigration = runMigration migrateAll
